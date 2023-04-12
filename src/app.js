0c3bcf3c778d4197b67b23780d9063079f59aeb7
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2023-04-12 01:29:57 -0300

    Alter cors accept

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ class App{
     middlewares(){
         this.express.use(express.json());
         this.express.use(cors({
-            origin: 'http://localhost:3000'
+            origin: 'https://beer-style.vercel.app/'
           }));
     }
 
