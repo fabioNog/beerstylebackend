4178f157f86591406de2e555ce01ceeb9181174c
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2023-04-12 20:41:29 -0300

    Fixed origin cors

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ class App{
     middlewares(){
         this.express.use(express.json());
         this.express.use(cors({
-            origin: 'https://beer-style.vercel.app/'
+            origin: ['http://localhost:3000', 'https://beer-style.vercel.app/']
           }));
     }
 
