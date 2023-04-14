ab6730b290efbe8fedeabdfdec4b6af240b42b6f
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2023-04-13 22:25:20 -0300

    Find a music on Spotify and alter req temperature

--- src/app/routes/BeerStyleRouter.js
+++ src/app/routes/BeerStyleRouter.js
@@ -1,24 +1,88 @@
-const BeerStyleRouter = require('express').Router();
-const BeerStyleController = require('../controllers/BeerStyleController');
-const AuthMiddleware = require('../middlewares/AuthMiddleware');
+const BeerStyleRouter = require("express").Router();
+const BeerStyleController = require("../controllers/BeerStyleController");
+const AuthMiddleware = require("../middlewares/AuthMiddleware");
+const axios = require("axios");
+const qs = require("qs");
 
-//CRUD -> CREATE, READ, UPDATE AND DELETE
-BeerStyleRouter.post('/beerstyle', BeerStyleController.create);
-BeerStyleRouter.get('/beerstyle', BeerStyleController.read);
+const SPOTIFY_API_URL = process.env.SPOTIFY_API_URL;
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
-BeerStyleRouter.patch('/beerstyle_temperature', async (req, res) => {
-    const { temperature } = req.body;
-    if (!temperature) {
-      return res.status(400).send('Temperature is required.');
-    }
-  
-    const beerStyle = await BeerStyleController.findBeerStyleByTemperature(temperature);
-  
-    return beerStyle;
+async function getAccessToken() {
+  const response = await axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${Buffer.from(
+        `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+    data: qs.stringify({
+      grant_type: "client_credentials",
+    }),
   });
+  console.log(response.data.access_token);
+  return response.data.access_token;
+}
 
-  BeerStyleRouter.delete('/beerstyle', BeerStyleController.deleteAll);
+function escolherEstilo(numero) {
+  let estilos = ["pop", "hip hop", "rock", "indie", "electronic", "country", "r&b", "jazz", "latin", "classical"];
+  let indice = Math.floor(numero * estilos.length)% 10;
+  return estilos[indice];
+}
 
+async function searchTrackByStyle(maxtemperature, accessToken) {
+  console.log(maxtemperature);
+  let estilo = escolherEstilo(maxtemperature);
+  console.log(estilo);
+  try {
+    const response = await axios.get(`${SPOTIFY_API_URL}/search`, {
+      params: {
+        q: `genre:"${estilo}"`,
+        type: "track",
+        limit: 1,
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (response.status === 200 && response.data.tracks.items.length > 0) {
+      return response.data.tracks.items[0];
+    } else {
+      throw new Error("No tracks found.");
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to search for tracks.");
+  }
+}
+//CRUD -> CREATE, READ, UPDATE AND DELETE
+BeerStyleRouter.post("/beerstyle", BeerStyleController.create);
+BeerStyleRouter.get("/beerstyle", BeerStyleController.read);
+
+BeerStyleRouter.get("/beerstyle_temperature", async (req, res) => {
+  const temperature = req.query.temperature;
+  if (!temperature) {
+    return res.status(400).send("Temperature is required.");
+  }
+  const beerStyle = await BeerStyleController.findBeerStyleByTemperature(
+    Number(temperature)
+  );
+
+  try {
+    console.log("Entrei no try")
+    const accessToken = await getAccessToken();
+     const track = await searchTrackByStyle(beerStyle.maxtemperature, accessToken);
+    return res.send(track,beerStyle);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Failed to search for track.");
+  }
+});
+
+BeerStyleRouter.delete("/beerstyle", BeerStyleController.deleteAll);
 
 // BeerStyleRouter.patch('/beerstyle/:id', BeerStyleController.update);
 
